d9655b07d1c54e929a901c985a314f0797a25f84
Rami Taleb <rtale089@uottawa.ca>
2016-11-24 16:35:31 -0500

    init

--- app/src/main/java/com/uottawa/cookr/MainActivity.java
+++ app/src/main/java/com/uottawa/cookr/MainActivity.java
@@ -2,12 +2,32 @@ package com.uottawa.cookr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.search_result);
+
+        String [] els = {"1 teapsoon Salt","2 teapsoon Pepper","3 Eggs","Cooking spray"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, els);
+
+        ListView list = (ListView) findViewById(R.id.LS1);
+
+        list.setAdapter(adapter);
+
+        String [] els2 = {"Step 1","Step 2","Step 3","Step 4"};
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, els2);
+
+        ListView list2 = (ListView) findViewById(R.id.LS2);
+
+        list2.setAdapter(adapter2);
     }
+
+
 }
