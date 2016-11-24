3670b0c0516ae4ab270a901c82ea0815e0b9dba4
Filip Slatinac <fslat092@uottawa.ca>
2016-11-23 20:01:12 -0500

    added more shit

--- app/src/main/java/com/uottawa/cookr/main_search.java
+++ app/src/main/java/com/uottawa/cookr/main_search.java
@@ -1,34 +1,37 @@
 package com.uottawa.cookr;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class main_search extends AppCompatActivity {
     final Context context = this;
     ListView list = null;
-    String [] times;
-    String [] cuisines;
-    String [] types;
+
+    selection_items currentList;
+
+    selection_items times;
+    selection_items cuisines;
+    selection_items types;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_search);
 
-        times = getResources().getStringArray(R.array.mealTime_Array);
-        cuisines = getResources().getStringArray(R.array.cuisine_Array);
-        types = getResources().getStringArray(R.array.type_Array);
+        times = new selection_items(getResources().getStringArray(R.array.mealTime_Array),"Meal Time");
+        cuisines = new selection_items(getResources().getStringArray(R.array.cuisine_Array),"Cuisines");
+        types = new selection_items(getResources().getStringArray(R.array.type_Array),"Type");
 
 
     }
@@ -39,20 +42,50 @@ public class main_search extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.single_listview_item,R.id.txtitem, els);
         list = new ListView(this);
         list.setAdapter(adapter);
+        list.setOnItemClickListener( new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int
+                    position, long id) {
+
+                ViewGroup vg=(ViewGroup)view;
+                TextView txt=(TextView)vg.findViewById(R.id.txtitem);
+
+                if(!currentList.isSelected(position)) {
+                    txt.setBackgroundResource(R.color.redPastel);
+                    currentList.select(position);
+                }
+
+                else{
+                    txt.setBackgroundResource(R.color.white);
+                    currentList.unselect(position);
+                }
+
+            }
+
+        });
+
     }
 
     public void showDialogListView(View view){
         String [] selection = {};
+        Button buttonUsed = null;
 
         switch (view.getId()){
             case R.id.cuisineButton:
-                selection = cuisines;
+                selection = cuisines.getArray();
+                currentList = cuisines;
+                buttonUsed = (Button) findViewById(R.id.cuisineButton);
                 break;
             case R.id.mealTimeButton:
-                selection = times;
+                selection = times.getArray();
+                currentList = times;
+                buttonUsed = (Button) findViewById(R.id.mealTimeButton);
                 break;
             case R.id.typeButton:
-                selection = types;
+                selection = types.getArray();
+                currentList = types;
+                buttonUsed = (Button) findViewById(R.id.typeButton);
                 break;
         }
 
@@ -60,7 +93,13 @@ public class main_search extends AppCompatActivity {
 
         AlertDialog.Builder builder=new AlertDialog.Builder(this);
         builder.setCancelable(true);
-        builder.setPositiveButton("Add",null);
+        final Button finalButtonUsed = buttonUsed;
+        builder.setPositiveButton("Select",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finalButtonUsed.setText(currentList.getSelectionText());
+                    }
+            });
         builder.setNegativeButton("Cancel",null);
 
 
