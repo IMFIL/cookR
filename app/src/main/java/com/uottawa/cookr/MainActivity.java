7a24a9eb05988328b68785ed7f08f665415a4a3d
Rami Taleb <rtale089@uottawa.ca>
2016-11-27 13:36:14 -0500

    scroll stuff

--- app/src/main/java/com/uottawa/cookr/MainActivity.java
+++ app/src/main/java/com/uottawa/cookr/MainActivity.java
@@ -2,6 +2,8 @@ package com.uottawa.cookr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_result);
 
-        String [] els = {"1 teapsoon Salt","2 teapsoon Pepper","3 Eggs","Cooking spray"};
+        String [] els = {"1 teapsoon Salt","2 teapsoon Pepper","3 Eggs","Cooking spray","1/4 cup Milk"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, els);
 
@@ -20,14 +22,35 @@ public class MainActivity extends AppCompatActivity {
 
         list.setAdapter(adapter);
 
-        String [] els2 = {"Step 1","Step 2","Step 3","Step 4"};
+        String [] els2 = {"Step 1","Step 2","Step 3","Step 4","Step 5","Step 6"};
 
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, els2);
 
         ListView list2 = (ListView) findViewById(R.id.LS2);
 
         list2.setAdapter(adapter2);
-    }
 
+        //Allows you to scroll the listview while you are in a scrollview layout
+        list.setOnTouchListener(new View.OnTouchListener() {
+            // Setting on Touch Listener for handling the touch inside ScrollView
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // Disallow the touch request for parent scroll on touch of child view
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+
+        //Allows you to scroll the listview while you are in a scrollview layout
+        list2.setOnTouchListener(new View.OnTouchListener() {
+            // Setting on Touch Listener for handling the touch inside ScrollView
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // Disallow the touch request for parent scroll on touch of child view
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+    }
 
 }
