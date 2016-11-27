7c5681e060d8f0933447b2d9fa0c422bc1aaf464
Filip Slatinac <fslat092@uottawa.ca>
2016-11-27 14:48:20 -0500

    fixed a confusing name

--- app/src/main/java/com/uottawa/cookr/SingleRecipeResult.java
+++ app/src/main/java/com/uottawa/cookr/SingleRecipeResult.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class SearchResult extends AppCompatActivity {
+public class SingleRecipeResult extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
