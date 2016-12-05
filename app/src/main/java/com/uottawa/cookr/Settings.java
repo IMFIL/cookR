6f5ab8bd1d38ee0805e2e35139a5fc48f9409474
anthonyanader <anthonyanader@gmail.com>
2016-12-04 19:55:22 -0500

    I think the Settings worked now

--- app/src/main/java/com/uottawa/cookr/Settings.java
+++ app/src/main/java/com/uottawa/cookr/Settings.java
@@ -7,7 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 
-class Settings extends AppCompatActivity {
+public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
